2fa89301b03bfe5a51ce9b8a342e6fedfde7f821
huangym07 <987297028@qq.com>
2025-07-01 19:56:11 +0800

    使用 TGA 处理库生成一张图片

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <util/tga_image.h>
 
-int main() {
-    TgaHeader tga_header;
+const TgaColor white{255, 255, 255, 255};
+const TgaColor red{0, 0, 255, 255};
 
-    std::cout << "TgaHeader size is " << sizeof(tga_header) << " bytes" << std::endl;
+int main() {
+    TgaImage tga_image(100, 100, TgaImage::RGB);
+
+    tga_image.set_pixel(50, 40, red);
+    tga_image.flip_vertically(); // 使原点在图像的左下角
+    tga_image.write_tga_file("output.tga");
 
     return 0;
 }
